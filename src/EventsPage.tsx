bdfb42a43898eaaebaa6a9884366ef6ec315b51a
Maciej Wolf <maciej.wolf@outlook.com>
2025-04-17 11:17:16 +0200

    updated tabs

--- src/EventsPage.tsx
+++ src/EventsPage.tsx
@@ -1,12 +1,35 @@
-import { Box, Tab, Tabs } from "@mui/material"
+import { Box, Tab, Tabs, Typography } from "@mui/material"
+import { useState } from "react";
 
 export const EventsPage = () => {
+  const [selectedTab, setSelectedTab] = useState(0);
+
+  const handleChange = (_event: React.SyntheticEvent, newValue: number) => {
+    setSelectedTab(newValue)
+  }
+
   return (
-    <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-      <Tabs>
-        <Tab label="Upcoming" />
-        <Tab label="On Demand" />
-      </Tabs>
+    <Box>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs value={selectedTab} onChange={handleChange}>
+          <Tab label="Upcoming" />
+          <Tab label="On Demand" />
+        </Tabs>
+      </Box>
+
+      <Box sx={{ mt: 2 }}>
+        {selectedTab === 0 && <UpcomingEvents />}
+        {selectedTab === 1 && <OnDemandEvents />}
+      </Box>
+
     </Box>
   )
+}
+
+const UpcomingEvents = () => {
+  return <Typography variant="h6">Upcoming Events</Typography>
+}
+
+const OnDemandEvents = () => {
+  return <Typography variant="h6">On Demand Events</Typography>
 }
