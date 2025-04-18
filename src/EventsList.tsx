fce89fb0f6379f1f9a89d58da345040c7353e2f4
Maciej Wolf <maciej.wolf@outlook.com>
2025-04-18 08:04:09 +0200

    filters panel placeholder

---
+++ src/EventsList.tsx
@@ -0,0 +1,32 @@
+import { Box, Chip, List, ListItem, ListItemText } from "@mui/material";
+import { observer } from "mobx-react-lite";
+import { store } from "./Store";
+import { TrackmanEvent } from "./TrackmanEvent";
+
+const getLocationLabel = (event: TrackmanEvent): string => {
+  if (event.locationType === "online") {
+    return "Online"
+  }
+  return `In person — ${event.location}`
+}
+
+export const EventsList = observer(() => {
+  return(
+    <>
+    <List>
+        {store.filteredUpcomingEvents.map(event => (
+          <ListItem key={event.id} divider>
+            <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+              <ListItemText
+                primary={event.name}
+                secondary={`${new Date(event.startDate).toLocaleString()} • ${getLocationLabel(event)}`}
+              />
+              {event.isPremium && <Chip label="Premium" color="warning" size="small" sx={{ marginLeft: 2 }}/>}
+            </Box>
+          </ListItem>
+        ))}
+      </List>
+    </>
+  )
+});
+
--- src/UpcomingEvents.tsx
+++ src/UpcomingEvents.tsx
@@ -1,37 +1,22 @@
-import { Box, Chip, CircularProgress, List, ListItem, ListItemText, Typography } from "@mui/material";
+import { Box, CircularProgress, Typography } from "@mui/material";
 import { observer } from "mobx-react-lite";
 import { store } from "./Store";
-import { TrackmanEvent } from "./TrackmanEvent";
-
-const getLocationLabel = (event: TrackmanEvent): string => {
-  if (event.locationType === "online") {
-    return "Online"
-  }
-  return `In person — ${event.location}`
-}
+import { EventsList } from "./EventsList";
+import { FiltersPanel } from "./FiltersPanel";
 
 export const UpcomingEvents = observer(() => {
-  console.log(store.isLoading);
   if (store.isLoading) {
     return <CircularProgress />
   }
 
   return (
-    <>
-      <Typography variant="h6" gutterBottom>Upcoming Events: {store.upcomingEvents.length}</Typography>
-      <List>
-        {store.filteredUpcomingEvents.map(event => (
-          <ListItem key={event.id} divider>
-            <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-              <ListItemText
-                primary={event.name}
-                secondary={`${new Date(event.startDate).toLocaleString()} • ${getLocationLabel(event)}`}
-              />
-              {event.isPremium && <Chip label="Premium" color="warning" size="small" sx={{ marginLeft: 2 }}/>}
-            </Box>
-          </ListItem>
-        ))}
-      </List>
-    </>
+    <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+      <Box>
+        <Typography variant="h6" gutterBottom>Upcoming Events: {store.upcomingEvents.length}</Typography>
+        <EventsList />
+      </Box>
+      <FiltersPanel />
+    </Box>
   )
 });
+
