c0f6a78a36a2ffaf0577b5212859a21ac3c569eb
Maciej Wolf <maciej.wolf@outlook.com>
2025-04-20 09:58:58 +0200

    refactor

--- src/utils/groupBy.ts
+++ src/utils/groupBy.ts
@@ -1,7 +1,10 @@
 export const groupBy = <T>(items: T[], keyFn: (item: T) => string): Record<string, T[]> => {
   return items.reduce((acc, item) => {
     const key = keyFn(item)
-    if (!acc[key]) acc[key] = []
+    
+    if (!acc[key]) 
+      acc[key] = []
+
     acc[key].push(item)
     return acc
   }, {} as Record<string, T[]>)
