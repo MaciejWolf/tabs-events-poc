6429694462851fa94d975c2b66c7666d3e75ac44
Maciej Wolf <maciej.wolf@outlook.com>
2025-04-20 20:32:42 +0200

    refactor

--- src/components/EventsPage/EventsList.tsx
+++ src/components/EventsPage/EventsList.tsx
@@ -1,5 +1,5 @@
 import { Box, Chip, List, ListItem, ListItemText } from "@mui/material";
-import { TrackmanEvent } from "../../types/TrackmanEvent";
+import { isUpcomingEvent, OnDemandEvent, TrackmanEvent, UpcomingEvent } from "../../types/TrackmanEvent";
 
 const getLocationLabel = (event: TrackmanEvent): string => {
   if (event.locationType === "online") {
@@ -12,18 +12,33 @@ export const EventsList = ({ events }: { events: TrackmanEvent[] }) => {
   return (
     <Box>
       <List>
-        {events.map(event => (
-          <ListItem key={event.id} divider>
-            <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-              <ListItemText
-                primary={event.name}
-                secondary={`${new Date(event.startDate).toLocaleString()} • ${getLocationLabel(event)}`}
-              />
-              {event.isPremium && <Chip label="Premium" color="warning" size="small" sx={{ marginLeft: 2 }} />}
-            </Box>
-          </ListItem>
-        ))}
+        {events.map(event => isUpcomingEvent(event)
+          ? <UpcomingEventRow event={event} />
+          : <OnDemandEventRow event={event} />
+        )}
       </List>
     </Box>
   )
 };
+
+const UpcomingEventRow = ({ event }: { event: UpcomingEvent }) =>
+  <ListItem key={event.id} divider>
+    <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+      <ListItemText
+        primary={event.name}
+        secondary={`${new Date(event.startDate).toLocaleString()} • ${getLocationLabel(event)}`}
+      />
+      {event.isPremium && <Chip label="Premium" color="warning" size="small" sx={{ marginLeft: 2 }} />}
+    </Box>
+  </ListItem>
+
+const OnDemandEventRow = ({ event }: { event: OnDemandEvent }) =>
+  <ListItem key={event.id} divider>
+    <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+      <ListItemText
+        primary={event.name}
+        secondary={`${new Date(event.startDate).toLocaleString()} • ${getLocationLabel(event)}`}
+      />
+      {event.isPremium && <Chip label="Premium" color="warning" size="small" sx={{ marginLeft: 2 }} />}
+    </Box>
+  </ListItem>
